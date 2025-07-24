a13fd0f3ad6af8ae5da67260e0254fc86a1d8124
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-07-24 15:07:07 -0400

    :building_construction: build: configuracao dos servicos da camada Infraestrutura

---
+++ gerenciamento_transacoes.Persistence/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using gerenciamento_transacoes.Application.Interfaces;
+using gerenciamento_transacoes.Persistence.Context;
+using gerenciamento_transacoes.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace gerenciamento_transacoes.Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("DatabaseUrl");
+            var database = configuration["ConnectionStrings:DatabaseName"];
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseMongoDB(connection, database);
+            });
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<ITransactionsRepository, TransactionRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
