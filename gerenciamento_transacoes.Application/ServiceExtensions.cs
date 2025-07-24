7f468bfc873f11d9dd8992802bf64730830e5ccb
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-07-24 14:33:42 -0400

    :building_construction: build: configuracao dos servicos da camada Aplicacao

---
+++ gerenciamento_transacoes.Application/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace gerenciamento_transacoes.Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
