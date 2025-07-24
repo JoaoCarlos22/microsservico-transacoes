a0eba15ddc5f9c12f0876a4a58b1831e06bd25b4
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-07-24 14:15:26 -0400

    :construction: implementacao da camada Dominio

---
+++ gerenciamento_transacoes.Domain/Entities/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using gerenciamento_transacoes.Domain.Common;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace gerenciamento_transacoes.Domain.Entities
+{
+    public sealed class Transaction : BaseEntity
+    {
+        [BsonElement("value")]
+        public double Value { get; set; }
+
+        [BsonElement("nameSender")]
+        public string NameSender { get; set; }
+
+        [BsonElement("nameReceiver")]
+        public string NameReceiver { get; set; }
+
+        [BsonElement("dateTime")]
+        public DateTime Date { get; set; }
+    }
+}
