49050d68586974044090a38c192f336eb28a18ba
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2025-07-24 20:50:26 -0400

    :wrench: build: configuracao do Azure Service Bus

--- gerenciamento_transacoes.Persistence/ServiceExtensions.cs
+++ gerenciamento_transacoes.Persistence/ServiceExtensions.cs
@@ -1,6 +1,10 @@
-﻿using gerenciamento_transacoes.Application.Interfaces;
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using gerenciamento_transacoes.Application.Interfaces;
+using gerenciamento_transacoes.Application.Interfaces.ServiceBus;
 using gerenciamento_transacoes.Persistence.Context;
 using gerenciamento_transacoes.Persistence.Repository;
+using gerenciamento_transacoes.Persistence.ServiceBus;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,13 +17,25 @@ namespace gerenciamento_transacoes.Persistence
         {
             var connection = configuration.GetConnectionString("DatabaseUrl");
             var database = configuration["ConnectionStrings:DatabaseName"];
+            var serviceBusNamespace = configuration["AzureServiceBus:Namespace"];
 
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseMongoDB(connection, database);
             });
+
+            services.AddSingleton(provider =>
+            {
+                var clientOptions = new ServiceBusClientOptions
+                {
+                    TransportType = ServiceBusTransportType.AmqpWebSockets
+                };
+                return new ServiceBusClient(serviceBusNamespace, new DefaultAzureCredential(), clientOptions);
+            });
+
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddScoped<ITransactionsRepository, TransactionRepository>();
+            services.AddScoped<ISenderMessage, SenderMessage>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
     }
